6e9af90711201fb0bbff1891f8957445eb03d3c1
aoi.ikeda <skmaoi.000@icloud.com>
2023-08-20 23:17:29 +0900

    question 4

--- sql/write_query/question4.sql
+++ sql/write_query/question4.sql
@@ -1,1 +1,2 @@
 -- 以下にクエリを書いてください
+select distinct post_tags.post_id, posts.content from tags join post_tags on tags.id = post_tags.tag_id join posts on post_tags.post_id = posts.id where tags.id = 6 order by post_id asc;
