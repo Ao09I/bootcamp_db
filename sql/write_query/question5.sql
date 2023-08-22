5461670f3505926c57b2e3a2d5baadb12bc4c6b9
aoi.ikeda <skmaoi.000@icloud.com>
2023-08-22 17:34:25 +0900

    question 5

--- sql/write_query/question5.sql
+++ sql/write_query/question5.sql
@@ -1,1 +1,2 @@
 -- 以下にクエリを書いてください
+select posts.id as post_id, posts.content, count(likes.id) as likes_count from posts left join likes on posts.id = likes.post_id group by posts.id order by likes_count desc, posts.posted_at desc;
